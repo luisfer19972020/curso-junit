cf44e6523e5f61544eaded1ac274feabac3f912c
luisfer19972020 <lcorrea.softsol@gmail.com>
2022-04-23 14:58:36 -0500

    video 8

--- src/main/java/com/curso/junit/udemy/App.java
+++ src/main/java/com/curso/junit/udemy/App.java
@@ -1,13 +1,33 @@
 package com.curso.junit.udemy;
 
+import java.math.BigDecimal;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        String a = "a";
+        String b = "b";
+        String c = "c";
+        System.out.println("Comparo B con A y da:");
+        System.out.println(b.compareTo(a));
+        System.out.println("Comparo B con B y da:");
+        System.out.println(b.compareTo(b));
+        System.out.println("Comparo B con C y da:");
+        System.out.println(b.compareTo(c));
+       
+        BigDecimal num1 = new BigDecimal("1");
+        BigDecimal num2 = new BigDecimal("2");
+        BigDecimal num3 = new BigDecimal("3");
+        System.out.println("Comparo 2 con 1 y da:");
+        System.out.println(num2.compareTo(num1));
+        System.out.println("Comparo 2 con 2 y da:");
+        System.out.println(num2.compareTo(num2));
+        System.out.println("Comparo 2 con 3 y da:");
+        System.out.println(num2.compareTo(num3));
+       
+       
     }
 }
--- src/test/java/com/curso/junit/udemy/models/CuentaTest.java
+++ src/test/java/com/curso/junit/udemy/models/CuentaTest.java
@@ -7,13 +7,21 @@ import java.math.BigDecimal;
 import org.junit.jupiter.api.Test;
 
 public class CuentaTest {
-    
+
     @Test
-    void test_nombre_cuenta(){
-        Cuenta cuenta= new Cuenta("Luis",new BigDecimal("4200.23"));
-        String esperado ="Luis";
+    void test_nombre_cuenta() {
+        Cuenta cuenta = new Cuenta("Luis", new BigDecimal("4200.23"));
+        String esperado = "Luis";
         String real = cuenta.getPersona();
         assertEquals(esperado, real);
         assertTrue(real.equals("Luis"));
+    }
+
+    @Test
+    void saldo_cuenta() {
+        Cuenta cuenta = new Cuenta("Luis", new BigDecimal("4200.23"));
+        assertEquals(4200.23, cuenta.getSaldo().doubleValue());
+        assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO)<0);//Valor saldo compare to con 0 sea -1(Simpre debe amyor igual a 0)
+        assertTrue(cuenta.getSaldo().compareTo(BigDecimal.ZERO)>0);//Valor saldo compare to con 0 sea -1(Simpre debe amyor igual a 0)
     }
 }
